2f9f7073a1106084e33c5e14471993d2c5c8d7b7
Francis Kogge <koggefa@gmail.com>
2023-03-01 19:05:26 -0800

    rearranged

--- src/main.cpp
+++ src/main.cpp
@@ -16,10 +16,10 @@ const int QUIT_OPTION = 4;
 void printIntro();
 bool fillFileContents(vector<string> &fileContents, const string &filename);
 bool invalidFileExtension(const string &filename);
+void run(const CodeSmellDetector &codeSmellDetector);
 void displayMainMenu();
 string selectMenuOption();
-bool isValidOption(string userInput);
-void run(const CodeSmellDetector &codeSmellDetector);
+bool isValidOption(const string &userInput);
 
 void printFunctionNames(const vector<string> &functionNames);
 void printLongMethodInfo(const CodeSmellDetector &codeSmellDetector);
@@ -57,63 +57,15 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void printFunctionNames(const vector<string> &functionNames) {
-    cout << "The file you provided contains the following methods: " << endl;
-    for (const string &name : functionNames) {
-        cout << "\t-> " << name << endl;
-    }
-}
-
-void printLongMethodInfo(const CodeSmellDetector &codeSmellDetector) {
-    vector<CodeSmellDetector::LongMethod> longMethodOccurrences = codeSmellDetector.getLongMethodOccurrences();
-
-    if (codeSmellDetector.hasLongMethodSmell()) {
-        for (const CodeSmellDetector::LongMethod &longMethod : longMethodOccurrences) {
-            cout << "The " << longMethod.functionName
-                 << " function is a " << CodeSmellDetector::smellTypeToString(longMethod.type)
-                 << ". It contains " << longMethod.lineCount << " lines of code. "
-                 << endl;
-        }
-    } else {
-        cout << "No function has Long Method!" << endl;
-    }
-}
-
-void printLongParameterListInfo(const CodeSmellDetector &codeSmellDetector) {
-    vector<CodeSmellDetector::LongParameterList> longParameterListOccurrences =
-            codeSmellDetector.getLongParameterListOccurrences();
-
-    if (codeSmellDetector.hasLongParameterListSmell()) {
-        for (const CodeSmellDetector::LongParameterList &occurrence : longParameterListOccurrences) {
-            cout << "The " << occurrence.functionName
-                 << " function has a " << CodeSmellDetector::smellTypeToString(occurrence.type)
-                 << ". It contains " << occurrence.parameterCount << " parameters. "
-                 << endl;
-        }
-    } else {
-        cout << "No function has Long Parameter List!" << endl;
-    }
-}
-
-void printDuplicatedCodeInfo(const CodeSmellDetector &codeSmellDetector) {
-    vector<CodeSmellDetector::DuplicatedCode> duplicatedCodeOccurrences =
-            codeSmellDetector.getDuplicateCodeOccurrences();
-
-    if (codeSmellDetector.hasDuplicateCodeSmell()) {
-        for (const CodeSmellDetector::DuplicatedCode &occurrence : duplicatedCodeOccurrences) {
-            cout << "The functions " << occurrence.functionNames.first << " and " << occurrence.functionNames.second
-                 << " are duplicated. Their similarity percentage is " << occurrence.similarityIndex * 100
-                 << endl;
-        }
-    } else {
-        cout << "No functions contain Duplicated Code!" << endl;
-    }
-}
-
 void printIntro() {
     cout << "Welcome to the Code Smell Detector program!" << endl;
     cout << "By Francis Kogge" << endl;
     cout << endl;
+}
+
+bool invalidFileExtension(const string &filename) {
+    size_t dotIndex = filename.find_last_of('.');
+    return dotIndex == string::npos || filename.substr(dotIndex) != ".cpp";
 }
 
 bool fillFileContents(vector<string> &fileContents, const string& filename) {
@@ -133,21 +85,13 @@ bool fillFileContents(vector<string> &fileContents, const string& filename) {
     return true;
 }
 
-void displayMainMenu() {
-    cout << "\nPlease choose one of the following options: " << endl;
-    cout << LONG_METHOD_OPTION << ". Long Method/Function Detection" << endl;
-    cout << LONG_PARAMETER_LIST_OPTION << ". Long Parameter List Detection" << endl;
-    cout << DUPLICATED_CODE_DETECTION_OPTION << ". Duplicated Code Detection" << endl;
-    cout << QUIT_OPTION << ". Quit" << endl;
-}
-
 void run(const CodeSmellDetector &codeSmellDetector) {
     printFunctionNames(codeSmellDetector.getFunctionNames());
 
-    int option = -1;
+    int option;
     string userInput;
 
-    while (option != QUIT_OPTION) {
+    do {
         do {
             displayMainMenu();
             userInput = selectMenuOption();
@@ -162,38 +106,15 @@ void run(const CodeSmellDetector &codeSmellDetector) {
         } else if (option == DUPLICATED_CODE_DETECTION_OPTION) {
             printDuplicatedCodeInfo(codeSmellDetector);
         }
-    }
+    } while (option != QUIT_OPTION);
 }
 
-bool isValidOption(string userInput) {
-    int option;
-    string errorMessage = userInput + " is not a valid option. \nPlease choose an integer between "
-                          + to_string(LONG_METHOD_OPTION) + " and " + to_string(QUIT_OPTION);
-
-    if (userInput.size() > 1) {
-        cout << errorMessage << endl;
-        return false;
-    }
-
-    for (const char &c : userInput) {
-        if (c == '.') {
-            cout << errorMessage << endl;
-            return false;
-        }
-    }
-
-    try {
-        option = stoi(userInput);
-    } catch (const exception &e) {
-        cout << errorMessage << endl;
-        return false;
-    }
-
-    bool isValid = option >= LONG_METHOD_OPTION && option <= QUIT_OPTION;
-    if (!isValid) {
-        cout << errorMessage << endl;
-    }
-    return isValid;
+void displayMainMenu() {
+    cout << "\nPlease choose one of the following options: " << endl;
+    cout << LONG_METHOD_OPTION << ". Long Method/Function Detection" << endl;
+    cout << LONG_PARAMETER_LIST_OPTION << ". Long Parameter List Detection" << endl;
+    cout << DUPLICATED_CODE_DETECTION_OPTION << ". Duplicated Code Detection" << endl;
+    cout << QUIT_OPTION << ". Quit" << endl;
 }
 
 string selectMenuOption() {
@@ -204,7 +125,91 @@ string selectMenuOption() {
     return option;
 }
 
-bool invalidFileExtension(const string &filename) {
-    size_t dotIndex = filename.find_last_of('.');
-    return dotIndex == string::npos || filename.substr(dotIndex) != ".cpp";
+bool isValidOption(const string &userInput) {
+    int option;
+    string errorMessage = userInput + " is not a valid option. \nPlease choose an integer between " +
+            to_string(LONG_METHOD_OPTION) + " and " + to_string(QUIT_OPTION);
+
+    // Size check
+    if (userInput.size() > 1) {
+        cout << errorMessage << endl;
+        return false;
+    }
+
+    // Decimal (float/double) check
+    for (const char &c : userInput) {
+        if (c == '.') {
+            cout << errorMessage << endl;
+            return false;
+        }
+    }
+
+    // Convert to integer
+    try {
+        option = stoi(userInput);
+    } catch (const exception &e) {
+        cout << errorMessage << endl;
+        return false;
+    }
+
+    // Range check
+    bool isValid = (option >= LONG_METHOD_OPTION && option <= QUIT_OPTION);
+    if (!isValid) {
+        cout << errorMessage << endl;
+    }
+
+    return isValid;
+}
+
+void printFunctionNames(const vector<string> &functionNames) {
+    cout << "The file you provided contains the following methods: " << endl;
+    for (const string &name : functionNames) {
+        cout << "\t-> " << name << endl;
+    }
+}
+
+void printLongMethodInfo(const CodeSmellDetector &codeSmellDetector) {
+    if (codeSmellDetector.hasLongMethodSmell()) {
+        vector<CodeSmellDetector::LongMethod> longMethodOccurrences =
+                codeSmellDetector.getLongMethodOccurrences();
+        for (const CodeSmellDetector::LongMethod &longMethod : longMethodOccurrences) {
+            cout << "The " << longMethod.functionName
+                 << " function is a " << CodeSmellDetector::smellTypeToString(longMethod.type)
+                 << ". It contains " << longMethod.lineCount << " lines of code. "
+                 << endl;
+        }
+    } else {
+        cout << "No function has Long Method!" << endl;
+    }
+}
+
+void printLongParameterListInfo(const CodeSmellDetector &codeSmellDetector) {
+    if (codeSmellDetector.hasLongParameterListSmell()) {
+        vector<CodeSmellDetector::LongParameterList> longParameterListOccurrences =
+                codeSmellDetector.getLongParameterListOccurrences();
+
+        for (const CodeSmellDetector::LongParameterList &occurrence : longParameterListOccurrences) {
+            cout << "The " << occurrence.functionName
+                 << " function has a " << CodeSmellDetector::smellTypeToString(occurrence.type)
+                 << ". It contains " << occurrence.parameterCount << " parameters. "
+                 << endl;
+        }
+    } else {
+        cout << "No function has Long Parameter List!" << endl;
+    }
+}
+
+void printDuplicatedCodeInfo(const CodeSmellDetector &codeSmellDetector) {
+    if (codeSmellDetector.hasDuplicateCodeSmell()) {
+        vector<CodeSmellDetector::DuplicatedCode> duplicatedCodeOccurrences =
+                codeSmellDetector.getDuplicateCodeOccurrences();
+
+        for (const CodeSmellDetector::DuplicatedCode &occurrence : duplicatedCodeOccurrences) {
+            cout << "The functions " << occurrence.functionNames.first << " and " << occurrence.functionNames.second
+                 << " are duplicated. Their similarity percentage is " << occurrence.similarityIndex * 100
+                 << endl;
+        }
+    } else {
+        cout << "No functions contain Duplicated Code!" << endl;
+    }
 }
